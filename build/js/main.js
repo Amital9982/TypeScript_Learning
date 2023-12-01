cfffc0e896882efc6aea37ee8015155b946cac3b
arpit <arpitmittal050603@gmail.com>
2023-12-01 07:59:51 +0530

    start

---
+++ build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let username = "Arpit mittal";
+console.log(username);
+let a = 12;
+let b = 6;
+let c = "2";
+console.log(a / b);
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+let username = "Arpit mittal";
+console.log(username);
+
+let a: number = 12;
+let b: number = 6;
+let c: string = "2";
+
+console.log(a / b);
